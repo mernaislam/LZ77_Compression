86dcced55780fcc80323695d820e9b8d85f6d41a
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-10-27 15:49:23 +0200

    Adding the first structure

---
+++ src/LZ77Algorithm.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LZ77Algorithm {
+
+    public static void startMenu(){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        switch (x){
+            case 1 -> System.out.println("Compression...");
+            case 2 -> System.out.println("Decompression...");
+            case 3 -> System.out.println("Thank you!");
+            default -> {
+                System.out.println("Invalid number, please choose between 1 - 3");
+                startMenu();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("LZ77 Algorithm - choose your required functionality [1-3]:");
+        System.out.println("1. Compression \n2. Decompression \n3. Exit");
+        startMenu();
+    }
+}
