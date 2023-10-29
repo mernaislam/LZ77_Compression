815e8a03c0bddc7ca2e8898717b232e68a35525b
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-10-29 19:01:31 +0200

    fixing compression algo

--- src/Compression.java
+++ src/Compression.java
@@ -3,84 +3,87 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Compression {
-    String searchWindow = "";
-    Integer tagPosition;
-    String content = "";
 
-//    public boolean checkSearchWindow(char c){
-//        for (int i = 0; i < searchWindow.length(); i++) {
-//            if(searchWindow.charAt(i) == c)
-//        }
-//    }
-
-    public int longestSequence(int i, int j){
-        // base case
-        if(i == content.length() || j == searchWindow.length()) return 0;
-        // take
-        int opt1 = -1;
-        if(content.charAt(i) == searchWindow.charAt(j)){
-            opt1 = longestSequence(i+1, j+1) + 1;
-        }
-
-        // leave
-        int opt2 = longestSequence(i, j+1);
-
-        return Math.max(opt1, opt2);
-    }
     public void compress(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the filename to compress: ");
         String fileName = sc.next();
         String fullPath = fileName + ".txt";
         Path path = Paths.get(fullPath);
-        try {
-            content = Files.readString(path, StandardCharsets.UTF_8);
-            int tagLength, pos;
-            String compressed = "";
-            for (int i = 0; i < content.length(); i++) {
+        String searchWindow = "";
+        int tagLength, pos, index;
+        String compressed = "";
+        Map<Integer, Integer> mp = new HashMap<>();
+        try
+        {
+            String content = Files.readString(path, StandardCharsets.UTF_8);
+            for (int i = 0; i < content.length(); i++)
+            {
+                mp.clear();
+                tagLength = 0;
                 pos = 0;
-                int index = i;
-                tagLength = longestSequence(i, 0);
-                System.out.print(tagLength + " ");
-//                boolean found = false;
-//                for (int j = 0; j < searchWindow.length(); j++) {
-//                    if(i == content.length()) break;
-//                    if(content.charAt(i) == searchWindow.charAt(j)){
-//                        // shift both
-//                        i++;
-//                        tagLength++;
-//                        if(pos == 0) pos = j;
-//                        found = true;
-//                    }
-//                    else if(found)
-//                    {
 //                        break;
-//                    }
-//                }
+                index = i;
+                boolean found = false, flag = false;
+                for (int j = 0; j < searchWindow.length(); j++)
+                {
+                    if(content.charAt(i) == searchWindow.charAt(j))
+                    {
+                        if(!found) pos = j;
+                        flag = true;
+                        i++;
+                        tagLength++;
+                        found = true;
+                    }
+                    else if(found)
+                    {
+                       mp.put(tagLength, pos);
+                       tagLength = 0;
+                       pos = 0;
+                       found = false;
+                       i = index;
+                    }
+                    if(found && (j == searchWindow.length()-1 || i == content.length())){
+                        mp.put(tagLength, pos);
+                        i = index;
+                    }
+                }
                 String tag;
-                if(tagLength == 0)
+                if(!flag)
                 {
                     tag = "<0,0," + content.charAt(i) + ">";
                     searchWindow += content.charAt(i);
                 }
                 else
                 {
-                    if(i == content.length()) break;
-                    tag = "<" + (index - pos) + "," + tagLength + ',' + content.charAt(i) + ">";
-                    for (int j = index; j <= index+tagLength; j++) {
-                        searchWindow+= content.charAt(j);
+                    int maxLength = 0, currentPos = 0;
+                    for (Map.Entry<Integer, Integer> entry : mp.entrySet()){
+                        if(maxLength < entry.getKey()){
+                            maxLength = entry.getKey();
+                            currentPos = entry.getValue();
+                        }
                     }
+                    tag = "<" + (index - currentPos) + "," + maxLength + ',' + content.charAt(i + maxLength) + ">";
+                    for (int j = index; j <= index+maxLength; j++)
+                    {
+                        searchWindow += content.charAt(j);
+                    }
+                    if(searchWindow.length() > 12){
+                        searchWindow = searchWindow.substring(searchWindow.length()-12);
+                    }
+                    i += maxLength;
                 }
                 compressed += tag;
             }
-            for (int i = 0; i < compressed.length(); i++) {
+            for (int i = 0; i < compressed.length(); i++)
+            {
                 System.out.print(compressed.charAt(i));
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
