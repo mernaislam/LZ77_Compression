c6adba561544e1488fa1090c1e38d8db26751b01
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-10-30 20:36:52 +0200

    Done with compressing files

--- src/Compression.java
+++ src/Compression.java
@@ -8,83 +8,82 @@ import java.util.*;
 public class Compression {
 
     public void compress(){
+        // open file
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the filename to compress: ");
         String fileName = sc.next();
         String fullPath = fileName + ".txt";
         Path path = Paths.get(fullPath);
+
+        // start compress
         String searchWindow = "";
         int tagLength, pos, index;
         String compressed = "";
-        Map<Integer, Integer> mp = new HashMap<>();
         try
         {
             String content = Files.readString(path, StandardCharsets.UTF_8);
             for (int i = 0; i < content.length(); i++)
             {
-                mp.clear();
                 tagLength = 0;
                 pos = 0;
-//                        break;
                 index = i;
-                boolean found = false, flag = false;
+                boolean found = false;
+                String matchChars = "";
                 for (int j = 0; j < searchWindow.length(); j++)
                 {
-                    if(content.charAt(i) == searchWindow.charAt(j))
+                    if(index >= content.length()) break;
+                    if(content.charAt(index) == searchWindow.charAt(j))
                     {
-                        if(!found) pos = j;
-                        flag = true;
-                        i++;
-                        tagLength++;
+                        matchChars += content.charAt(index);
                         found = true;
+                        index++;
                     }
                     else if(found)
                     {
-                       mp.put(tagLength, pos);
-                       tagLength = 0;
-                       pos = 0;
-                       found = false;
-                       i = index;
+                        if(matchChars.length() > tagLength)
+                        {
+                            tagLength = matchChars.length();
+                            pos = searchWindow.lastIndexOf(matchChars);
+                        }
+                        matchChars = "";
+                        index = i;
                     }
-                    if(found && (j == searchWindow.length()-1 || i == content.length())){
-                        mp.put(tagLength, pos);
-                        i = index;
+                }
+                if(!matchChars.isEmpty()){
+                    if(matchChars.length() > tagLength)
+                    {
+                        tagLength = matchChars.length();
+                        pos = searchWindow.lastIndexOf(matchChars);
                     }
                 }
                 String tag;
-                if(!flag)
+                if(!found)
                 {
                     tag = "<0,0," + content.charAt(i) + ">";
                     searchWindow += content.charAt(i);
                 }
                 else
                 {
-                    int maxLength = 0, currentPos = 0;
-                    for (Map.Entry<Integer, Integer> entry : mp.entrySet()){
-                        if(maxLength < entry.getKey()){
-                            maxLength = entry.getKey();
-                            currentPos = entry.getValue();
-                        }
-                    }
-                    tag = "<" + (index - currentPos) + "," + maxLength + ',' + content.charAt(i + maxLength) + ">";
-                    for (int j = index; j <= index+maxLength; j++)
-                    {
+                    tag = "<" + (i - pos) + "," + tagLength + ',';
+                    if(i + tagLength >= content.length()) tag += "null" + ">";
+                    else tag += content.charAt(i + tagLength) + ">";
+                    for (int j = i; j <= i + tagLength && j < content.length(); j++) {
                         searchWindow += content.charAt(j);
                     }
-                    if(searchWindow.length() > 12){
-                        searchWindow = searchWindow.substring(searchWindow.length()-12);
-                    }
-                    i += maxLength;
+                    i += tagLength;
                 }
                 compressed += tag;
             }
-            for (int i = 0; i < compressed.length(); i++)
-            {
-                System.out.print(compressed.charAt(i));
-            }
+            String compressedFile = fileName + "_compressed.txt";
+            Path pathCompressed = Paths.get(compressedFile);
+            Files.writeString(pathCompressed, compressed, StandardCharsets.UTF_8);
+            System.out.println("File compressed successfully in " + compressedFile);
         }
         catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Error: File does not exist");
+            System.out.print("Do you want to try again? (y/n): ");
+            char choice = sc.next().charAt(0);
+            if(choice == 'Y' || choice == 'y') compress();
         }
     }
 }
--- src/LZ77Algorithm.java
+++ src/LZ77Algorithm.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class LZ77Algorithm {
 
     public static void startMenu(){
+        System.out.println("1. Compression \n2. Decompression \n3. Exit");
         Scanner sc = new Scanner(System.in);
         Compression c = new Compression();
         int x = sc.nextInt();
         switch (x){
-            case 1 -> c.compress();
+            case 1 -> {
+                c.compress();
+                System.out.print("Do you want to perform another operation? (y/n): ");
+                char choice = sc.next().charAt(0);
+                if(choice == 'Y' || choice == 'y') startMenu();
+            }
             case 2 -> System.out.println("Decompression...");
             case 3 -> System.out.println("Thank you!");
             default -> {
@@ -18,7 +24,6 @@ public class LZ77Algorithm {
     }
     public static void main(String[] args) {
         System.out.println("LZ77 Algorithm - choose your required functionality [1-3]:");
-        System.out.println("1. Compression \n2. Decompression \n3. Exit");
         startMenu();
     }
 }
